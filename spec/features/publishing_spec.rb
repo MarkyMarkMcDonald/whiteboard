90f9d8c5f9340031094855665a161685540ebfce
Daphne Lin and Sarah Michaelson <pair+dalin+smichaelson@pivotallabs.com>
2016-01-18 15:58:10 -0500

    Update spec to match new placeholder text

--- spec/features/publishing_spec.rb
+++ spec/features/publishing_spec.rb
@@ -14,11 +14,11 @@ describe "publishing", js: true do
     expect_any_instance_of(WordpressService).to_not receive(:send!)
     click_link(standup.title)
 
-    fill_in "Blogger Name(s)", with: "Me"
-    fill_in "Post Title (eg: Best Standup Ever)", with: "empty post"
+    fill_in "Standup Host(s)", with: "Me"
+    fill_in "Email Subject (eg: Best Standup Ever)", with: "empty post"
 
     accept_confirm do
-      click_on "Create Post"
+      click_on "Send Email"
     end
 
     expect(page).to have_content("Please update these items with any new information from standup:")
@@ -57,11 +57,11 @@ describe "publishing", js: true do
     click_on 'Post to Blog'
     click_button 'Create Item'
 
-    fill_in "Blogger Name(s)", with: "Me"
-    fill_in "Post Title (eg: Best Standup Ever)", with: "empty post"
+    fill_in "Standup Host(s)", with: "Me"
+    fill_in "Email Subject (eg: Best Standup Ever)", with: "empty post"
 
     accept_confirm do
-      click_on "Create Post"
+      click_on "Send Email"
     end
 
     click_on 'Post Blog Entry'
@@ -86,11 +86,11 @@ describe "publishing", js: true do
     click_on 'Post to Blog'
     click_button 'Create Item'
 
-    fill_in "Blogger Name(s)", with: "Me"
-    fill_in "Post Title (eg: Best Standup Ever)", with: "empty post"
+    fill_in "Standup Host(s)", with: "Me"
+    fill_in "Email Subject (eg: Best Standup Ever)", with: "empty post"
 
     accept_confirm do
-      click_on "Create Post"
+      click_on "Send Email"
     end
 
     click_on 'Post Blog Entry'
